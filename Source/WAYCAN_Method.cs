492ae55f8ee49d365a2c190bfd1d94e0c1b6f94f
Samuel Bucher <maxwellgrey853@gmail.com>
2024-09-02 20:40:29 +0300

    init

---
+++ Source/WAYCAN_Method.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace WAYCAN
+{
+    public class WAYCAN_Method
+    {
+        public static GraphicMeshSet ifNarrowMesh(Pawn pawn)
+        {
+            if (pawn.story.headType.narrow)
+                return HumanlikeMeshPoolUtility.GetHumanlikeHeadSetForPawn(pawn, 0.85f, 0.85f); ;
+            return HumanlikeMeshPoolUtility.GetHumanlikeHeadSetForPawn(pawn);
+        }
+
+    }
+}
---
+++ Source/WAYCAN_Patches.cs
@@ -0,0 +1,43 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using Verse;
+using System.Reflection;
+
+namespace WAYCAN
+{
+    [StaticConstructorOnStartup]
+    public class HarmonyPatches
+    {
+        private static readonly MethodInfo isNarrowMesh = AccessTools.Method(typeof(WAYCAN_Method), nameof(WAYCAN_Method.ifNarrowMesh), new Type[] { typeof(Verse.Pawn) });
+        private static readonly MethodInfo getHumanlikeHeadSetForPawn = AccessTools.Method(typeof(HumanlikeMeshPoolUtility), nameof(HumanlikeMeshPoolUtility.GetHumanlikeHeadSetForPawn), new Type[] { typeof(Verse.Pawn), typeof(float), typeof(float) });
+
+        static HarmonyPatches()
+        {
+            Harmony harmony = new Harmony(id: "rimworld.SamBucher.WAYCAN");
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(PawnRenderNode_Apparel), nameof(PawnRenderNode_Apparel.MeshSetFor))]
+        public static class MeshSetFor_Apparel_Patch
+        {
+            [HarmonyTranspiler]
+            public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                List<CodeInstruction> code = new List<CodeInstruction>(instructions);
+
+                for (int i = 0; i < code.Count; i++)
+                {
+                    if (code[i].opcode == OpCodes.Ldc_R4 && code[i + 2].Calls(getHumanlikeHeadSetForPawn))
+                    {
+                        yield return new CodeInstruction(OpCodes.Call, isNarrowMesh);
+                        i += 2;
+                    }
+                    else
+                        yield return code[i];
+                }
+            }
+        }
+    }
+}
